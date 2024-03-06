9d41f99577ce71093de1da11ffee7d38825172be
Samiya Ayub <159205998+Samiyaayub@users.noreply.github.com>
2024-03-07 03:30:09 +0500

    Add files via upload

---
+++ assignment 3/main.js
@@ -0,0 +1,6 @@
+"use strict";
+//Question 3: Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+let personName = "Samiya Ayub";
+console.log(personName.toLowerCase());
+console.log(personName.toUpperCase());
+console.log(personName.charAt(0).toUpperCase() + personName.slice(1).toLowerCase());
