f0ab5280f1369f1f5dc49512a395a83b0002aa7f
Samiya Ayub <159205998+Samiyaayub@users.noreply.github.com>
2024-03-07 04:41:15 +0500

    Add files via upload

---
+++ assignment 5/main.js
@@ -0,0 +1,5 @@
+"use strict";
+//Question 5: Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+let famousPerson = "ROY T. BENNET";
+let message = `${famousPerson} once said, "DO WHAT IS RIGHT, NOT WHAT IS EASY NOR WHAT IS POPULAR."`;
+console.log(message);
--- assignment 5/main.ts
+++ assignment 5/main.ts
@@ -1,1 +1,6 @@
+//Question 5: Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
 
+
+let famousPerson: string = "ROY T. BENNET";
+let message: string =`${famousPerson} once said, "DO WHAT IS RIGHT, NOT WHAT IS EASY NOR WHAT IS POPULAR."`;
+ console.log(message);
