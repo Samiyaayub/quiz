d32ae228ac062095f37fc9053610660f60dd8e87
Samiya Ayub <159205998+Samiyaayub@users.noreply.github.com>
2024-03-07 03:28:26 +0500

    Add files via upload

---
+++ assignment 2/main.js
@@ -0,0 +1,4 @@
+"use strict";
+// Question 2: Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+let Name = "Samiya";
+console.log(`Hello ${Name}, would you like to learn some Typescript today?`);
--- assignment 2/main.ts
+++ assignment 2/main.ts
@@ -1,1 +1,5 @@
+// Question 2: Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
 
+
+let Name:string = "Samiya";
+console.log(`Hello ${Name}, would you like to learn some Typescript today?`);
