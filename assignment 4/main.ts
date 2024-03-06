a1e065ef5b42150825f3746869c9c9aef78a7929
Samiya Ayub <159205998+Samiyaayub@users.noreply.github.com>
2024-03-07 04:30:33 +0500

    Add files via upload

--- assignment 4/main.ts
+++ assignment 4/main.ts
@@ -1,1 +1,4 @@
+//Questin 4: Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:Albert Einstein once said, “A person who never made a mistake never tried anything new.”
 
+
+console.log('Sir Francis Bacon once said, "Knowledge Is Power."');
