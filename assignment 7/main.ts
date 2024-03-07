3589289b1406a6239bce587605b75de9755d23d1
Samiya Ayub <159205998+Samiyaayub@users.noreply.github.com>
2024-03-07 05:45:00 +0500

    Add files via upload

--- assignment 7/main.ts
+++ assignment 7/main.ts
@@ -1,1 +1,10 @@
+//Question 7: Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.const byAddition = 4 + 4;
 
+
+console.log(4 + 4); //Addition
+
+console.log(10 - 2); //Subtraction
+
+console.log(4 * 2); //Multiplication
+
+console.log(16 / 2); //Division
