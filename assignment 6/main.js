83114ab6baf0d2a75f5a7a06c1c80b5d6338c144
Samiya Ayub <159205998+Samiyaayub@users.noreply.github.com>
2024-03-07 05:02:32 +0500

    Add files via upload

---
+++ assignment 6/main.js
@@ -0,0 +1,5 @@
+"use strict";
+//Question 6: Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name. Make sure you use each character combination, "\t" and "\n", at least once. Print the name once, so the whitespace around the name is displayed. Then print the name after striping the white spaces.
+let personName = "\t\n Samiya \t\n";
+console.log(personName);
+console.log(personName.trim());
