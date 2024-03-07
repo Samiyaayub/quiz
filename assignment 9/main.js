772f45717d34906f5baf24856989428e21fa4b3f
Samiya Ayub <159205998+Samiyaayub@users.noreply.github.com>
2024-03-07 06:06:45 +0500

    Add files via upload

---
+++ assignment 9/main.js
@@ -0,0 +1,5 @@
+"use strict";
+//Question 9: Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+let favorite_number = 21;
+let message = `My Favourite Numer is ${favorite_number}. Because It's My Birth Date.`;
+console.log(message);
--- assignment 9/main.ts
+++ assignment 9/main.ts
@@ -1,1 +1,5 @@
+//Question 9: Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
 
+let favorite_number: number = 21;
+let message = `My Favourite Numer is ${favorite_number}. Because It's My Birth Date.` ;
+console.log(message);
