89adcf52d1ad2f7dc0a21326451ad1a834bc2bb4
Samiya Ayub <159205998+Samiyaayub@users.noreply.github.com>
2024-03-07 07:09:43 +0500

    Add files via upload

---
+++ assignment 11/main.js
@@ -0,0 +1,6 @@
+"use strict";
+//Question11. Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
+let names = ["Jack", "Milo", "Emre"];
+for (let i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
--- assignment 11/main.ts
+++ assignment 11/main.ts
@@ -1,1 +1,6 @@
+//Question11. Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
 
+let names: string[] = ["Jack" , "Milo" , "Emre"];
+for (let i = 0; i < names.length; i++){
+    console.log(names[i]);
+}
