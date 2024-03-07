ec9b24b32cc62ceb557b41c2a1bb54b46811df88
Samiya Ayub <159205998+Samiyaayub@users.noreply.github.com>
2024-03-07 06:48:04 +0500

    Add files via upload

---
+++ assignment 10/main.js
@@ -0,0 +1,6 @@
+"use strict";
+// Question10. Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does
+// Samiya, 2024-03-07
+// 100 days of coding challenge
+let myName = "Samiya";
+console.log(`I'm ${myName}, I really enjoy the 100 days of coding challenge.`);
