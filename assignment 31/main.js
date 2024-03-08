d8e3f8c54046de3c787efe784a5072edccc37b8e
Samiya Ayub <159205998+Samiyaayub@users.noreply.github.com>
2024-03-09 02:08:52 +0500

    Add files via upload

---
+++ assignment 31/main.js
@@ -0,0 +1,9 @@
+"use strict";
+// No Users: Add an if test to Exercise 28 to make sure the list of users is not empty.
+// • If the list is empty, print the message We need to find some users!
+Object.defineProperty(exports, "__esModule", { value: true });
+// • Remove all of the usernames from your array, and make sure the correct message is printed
+let user = [''];
+if (user.includes('')) {
+    console.log("We need to find some users!\n");
+}
--- assignment 31/main.ts
+++ assignment 31/main.ts
@@ -1,1 +1,9 @@
+// No Users: Add an if test to Exercise 28 to make sure the list of users is not empty.
+// • If the list is empty, print the message We need to find some users!
 
+// • Remove all of the usernames from your array, and make sure the correct message is printed
+
+let user:string[] = ['']
+if(user.includes('')) {
+     console.log("We need to find some users!\n");
+}
