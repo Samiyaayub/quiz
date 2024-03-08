12ab44e9004a329e43a0480fd9bf17a243268294
Samiya Ayub <159205998+Samiyaayub@users.noreply.github.com>
2024-03-09 01:41:29 +0500

    Add files via upload

--- assignment 44/main.ts
+++ assignment 44/main.ts
@@ -1,1 +1,13 @@
+//Question 44: Sandwiches: Write a function that accepts a array of items a person wants on a sandwich. The function should have one parameter that collects as many items as the function call provides, and it should print a summary of the sandwich that is being ordered. Call the function three times, using a different number of arguments each time.
 
+function makeSandwiches(...items:string[]) {
+    console.log("Sandwitch Order:");
+    for(let i = 0; i < items.length; i++) {
+      console.log(`- ${items[i]}`);
+    }
+    console.log("Enjoy your Sandwich!");
+}
+
+makeSandwiches('Lettuce', 'Tomato', 'Cheese');
+makeSandwiches('Ham', 'Mayo');
+makeSandwiches('Chicken');
