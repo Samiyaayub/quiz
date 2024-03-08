28f006db8257a1b3201a9d215b29d7e4d112f519
Samiya Ayub <159205998+Samiyaayub@users.noreply.github.com>
2024-03-08 22:46:19 +0500

    Add files via upload

---
+++ assignment 20/main.js
@@ -0,0 +1,5 @@
+"use strict";
+// Question:20 Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+Object.defineProperty(exports, "__esModule", { value: true });
+let countries = ["Mexico", "Italy", "Denmark", "United States"];
+console.log("Countries I like to visit:", countries);
--- assignment 20/main.ts
+++ assignment 20/main.ts
@@ -1,1 +1,4 @@
+// Question:20 Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
 
+let countries:string[] = ["Mexico", "Italy", "Denmark", "United States"];
+console.log("Countries I like to visit:", countries);
