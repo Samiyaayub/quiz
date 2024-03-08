e240d712a114a8e402dcccd0f425a4fe922789cf
Samiya Ayub <159205998+Samiyaayub@users.noreply.github.com>
2024-03-08 22:26:44 +0500

    Add files via upload

--- assignment 13/main.ts
+++ assignment 13/main.ts
@@ -1,1 +1,8 @@
+//13. Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”
 
+
+let cars: string [] = ["Honda city" , "Honda Insight" , "Honda Accord" , "Civic"];
+cars.forEach (cars => {
+console.log(`I would like to own a ${cars}.`)
+}
+    )
