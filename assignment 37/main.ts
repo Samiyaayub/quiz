e262f18ad562941ae56b6cf067993233a8f406a4
Samiya Ayub <159205998+Samiyaayub@users.noreply.github.com>
2024-03-09 04:23:14 +0500

    Add files via upload

--- assignment 37/main.ts
+++ assignment 37/main.ts
@@ -1,1 +1,9 @@
+//Question 37: Large Shirts: Modify the make_shirt() function so that shirts are large by default with a message that reads I love TypeScript. Make a large shirt and a medium shirt with the default message, and a shirt of any size with a different message.
 
+function makeShirt(size: number, msg:string) {
+    console.log(`Size of Shirt: ${size}\n${msg}`);
+}      
+
+makeShirt(40, 'I Love Typescript.(default msgs with too much large size).\n');
+makeShirt(32, '"Be kind, for everyone\nYou meet is\nFighting a battle\nYou know nothing about"\n');
+makeShirt(28, '\tBe the Change\n\tYou Hope to See\n\tIn the World.');
