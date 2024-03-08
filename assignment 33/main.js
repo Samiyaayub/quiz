6dd38e700abfa9b5fa3bc6bf5a474d6072227156
Samiya Ayub <159205998+Samiyaayub@users.noreply.github.com>
2024-03-09 02:43:28 +0500

    Add files via upload

---
+++ assignment 33/main.js
@@ -0,0 +1,23 @@
+"use strict";
+//Question:33 Ordinal Numbers: Ordinal numbers indicate their position in a array, such as 1st or 2nd. Most ordinal numbers end in th, except 1, 2, and 3.
+// • Store the numbers 1 through 9 in a array.
+// • Loop through the array.
+// • Use an if-else chain inside the loop to print the proper ordinal ending for each number. Your output should read "1st 2nd 3rd 4th 5th 6th 7th 8th 9th", and each result should be on a separate line.
+Object.defineProperty(exports, "__esModule", { value: true });
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+for (let num of numbers) {
+    let OrdinalNumbers;
+    if (num === 1) {
+        OrdinalNumbers = "st";
+    }
+    else if (num === 2) {
+        OrdinalNumbers = "nd";
+    }
+    else if (num === 3) {
+        OrdinalNumbers = "rd";
+    }
+    else {
+        OrdinalNumbers = "th";
+    }
+    console.log(`${num}${OrdinalNumbers}`);
+}
