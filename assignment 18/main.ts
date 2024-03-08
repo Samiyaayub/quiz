3430fa87240f6c174c193cff744a828198b8194d
Samiya Ayub <159205998+Samiyaayub@users.noreply.github.com>
2024-03-08 22:41:55 +0500

    Add files via upload

--- assignment 18/main.ts
+++ assignment 18/main.ts
@@ -1,1 +1,44 @@
+// Question18: Seeing the World: Think of at least five places in the world you’d like to visit.
+// • Store the locations in a array. Make sure the array is not in alphabetical order.
 
+// • Print your array in its original order.
+
+// • Print your array in alphabetical order without modifying the actual list.
+
+// • Show that your array is still in its original order by printing it.
+
+// • Print your array in reverse alphabetical order without changing the order of the original list.
+
+// • Show that your array is still in its original order by printing it again.
+
+// • Reverse the order of your list. Print the array to show that its order has changed.
+
+// • Reverse the order of your list again. Print the list to show it’s back to its original order.
+
+// • Sort your array so it’s stored in alphabetical order. Print the array to show that its order has been changed.
+
+// • Sort to change your array so it’s stored in reverse alphabetical order. Print the list to show that its order has changed.
+
+let places:string[] = ["Bali", "Abu Dhabi", "Cappadocia", "Tulum"];
+
+console.log("Original Order:", places);
+
+console.log("Alphabetical Order:", [...places].sort());
+
+console.log("original order:", places);
+
+console.log("Reverse alphabetical order:", [...places].sort().reverse());
+
+console.log("original order:", places);
+
+places.reverse();
+console.log("Reversed order:", places);
+
+places.reverse();
+console.log("Original order:", places);
+
+places.sort();
+console.log("Alphabetical order:", places);
+
+places.reverse();
+console.log("Reverse alphabetical order:", places);
