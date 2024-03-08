8e0ae9fa168f63631efaeff07c6348583effc16b
Samiya Ayub <159205998+Samiyaayub@users.noreply.github.com>
2024-03-09 01:45:03 +0500

    Add files via upload

---
+++ assignment 45/main.js
@@ -0,0 +1,20 @@
+"use strict";
+//Question 45: Cars: Write a function that stores information about a car in a Object. The function should always receive a manufacturer and a model name. It should then accept an arbitrary number of keyword arguments. Call the function with the required information and two other name-value pairs, such as a color or an optional feature. Print the Object that’s returned to make sure all the information was stored correctly.
+Object.defineProperty(exports, "__esModule", { value: true });
+function makeCar(manufacturer, model, ...options) {
+    let car = {
+        manufacturer: manufacturer,
+        model: model,
+        options: {}
+    };
+    for (let option of options) {
+        for (let key in option) {
+            car.options[key] = option[key];
+        }
+    }
+    return car;
+}
+let car1 = makeCar('Toyato', 'Corola', { color: 'blue' }, { sunroof: true });
+let car2 = makeCar('Royal-royce', 'Phantom', { color: 'Black' });
+let car3 = makeCar('Supra', 'Toyato GR', { color: 'blue' }, { sunroof: true });
+console.log(car1, "\n", car2, "\n", car3);
