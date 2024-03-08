e881d6815ee7147da54ae999526719bcad59e9f5
Samiya Ayub <159205998+Samiyaayub@users.noreply.github.com>
2024-03-09 04:52:00 +0500

    Add files via upload

---
+++ assignment 39/main.js
@@ -0,0 +1,11 @@
+"use strict";
+// Question 39: City Names: Write a function called city_country() that takes in the name of a city and its country. The function should return a string formatted like this:
+Object.defineProperty(exports, "__esModule", { value: true });
+// "Lahore, Pakistan"
+// Call your function with at least three city-country pairs, and print the value that’s returned.
+function city_country(city, country) {
+    console.log(`"${city}, ${country}"`);
+}
+city_country('Lahore', 'Pakistan');
+city_country('Istanbul', 'Turkey');
+city_country('Mungyeong', 'Korea');
--- assignment 39/main.ts
+++ assignment 39/main.ts
@@ -1,1 +1,13 @@
+// Question 39: City Names: Write a function called city_country() that takes in the name of a city and its country. The function should return a string formatted like this:
 
+// "Lahore, Pakistan"
+
+// Call your function with at least three city-country pairs, and print the value that’s returned.
+
+function city_country(city: string, country:string) {
+    console.log(`"${city}, ${country}"`);
+}
+
+city_country('Lahore','Pakistan');
+city_country('Istanbul', 'Turkey');
+city_country('Mungyeong', 'Korea');
