3210aa9e99028f0ab46d6089d2d5f2941585b195
Samiya Ayub <159205998+Samiyaayub@users.noreply.github.com>
2024-03-08 22:30:26 +0500

    Add files via upload

---
+++ assignment 14/main.js
@@ -0,0 +1,7 @@
+"use strict";
+// 14. Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner.
+Object.defineProperty(exports, "__esModule", { value: true });
+let guest = ['Olivia', 'Lily', 'Sophia', 'Elena'];
+guest.forEach(guest => {
+    console.log(`Dear ${guest}, You're Invited To a Dinner.`);
+});
--- assignment 14/main.ts
+++ assignment 14/main.ts
@@ -1,1 +1,6 @@
+// 14. Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner.
 
+let guest: string[] = ['Olivia' , 'Lily' , 'Sophia' ,'Elena'];
+guest.forEach  (guest => {
+        console.log(`Dear ${guest}, You're Invited To a Dinner.`!)}
+    );
