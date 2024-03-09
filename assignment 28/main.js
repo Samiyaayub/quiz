848e34996b39b2c815eab6b19f2b298d59c61c08
Samiya Ayub <159205998+Samiyaayub@users.noreply.github.com>
2024-03-09 05:49:57 +0500

    Add files via upload

---
+++ assignment 28/main.js
@@ -0,0 +1,28 @@
+"use strict";
+// Stages of Life: Write an if-else chain that determines a person’s stage of life. Set a value for the variable age, and then:
+// • If the person is less than 2 years old, print a message that the person is a baby.
+Object.defineProperty(exports, "__esModule", { value: true });
+// • If the person is at least 2 years old but less than 4, print a message that the person is a toddler.
+// • If the person is at least 4 years old but less than 13, print a message that the person is a kid.
+// • If the person is at least 13 years old but less than 20, print a message that the person is a teenager.
+// • If the person is at least 20 years old but less than 65, print a message that the person is an adult.
+// • If the person is age 65 or older, print a message that the person is an elder.
+let person_age = 17;
+if (person_age < 2) {
+    console.log("Baby");
+}
+else if (person_age < 4) {
+    console.log("Toddler");
+}
+else if (person_age < 13) {
+    console.log("Kid");
+}
+else if (person_age < 20) {
+    console.log("Teenager");
+}
+else if (person_age < 65) {
+    console.log("Adult");
+}
+else {
+    console.log("Elder");
+}
