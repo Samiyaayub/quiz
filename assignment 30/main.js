3d4d7e02f689177a53b3fb1e38133c98cbe690dc
Samiya Ayub <159205998+Samiyaayub@users.noreply.github.com>
2024-03-09 15:52:02 +0500

    Add files via upload

---
+++ assignment 30/main.js
@@ -0,0 +1,14 @@
+"use strict";
+//Question 30: Hello Admin: Make a array of five or more usernames, including the name 'admin'. Imagine you are writing code that will print a greeting to each user after they log in to a website. Loop through the array, and print a greeting to each user:
+// • If the username is 'admin', print a special greeting, such as Hello admin, would you like to see a status report?
+Object.defineProperty(exports, "__esModule", { value: true });
+// • Otherwise, print a generic greeting, such as Hello Eric, thank you for logging in again.
+let employee = ["admin", "Olivia", "Isabella", "Camila", "Daisy"];
+for (let i = 0; i < employee.length; i++) {
+    if (employee[i] == "admin") {
+        console.log("Hello admin, would you like to see a status report?");
+    }
+    else {
+        console.log(`Hello ${employee[i]}`);
+    }
+}
