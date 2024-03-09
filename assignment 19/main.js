8b07846d874f7311d7f5c4a3de8f9d1873641526
Samiya Ayub <159205998+Samiyaayub@users.noreply.github.com>
2024-03-09 16:27:24 +0500

    Add files via upload

---
+++ assignment 19/main.js
@@ -0,0 +1,5 @@
+"use strict";
+// Question 19: Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
+Object.defineProperty(exports, "__esModule", { value: true });
+let guests = ['Olivia', 'Lily', 'Sophia', 'Elena', 'Oscar', 'William', 'Tokyo', 'Michael'];
+console.log(`I'm inviting ${guests.length} people to dinner.`);
