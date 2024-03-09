5404382a89d8b788b571902fa65e7ba0aadac531
Samiya Ayub <159205998+Samiyaayub@users.noreply.github.com>
2024-03-09 16:18:30 +0500

    Add files via upload

---
+++ assignment 23/main.js
@@ -0,0 +1,39 @@
+"use strict";
+//Question 23: Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test. Your code should look something like this:
+Object.defineProperty(exports, "__esModule", { value: true });
+// let car = 'subaru';
+// console.log("Is car == 'subaru'? I predict True.")
+// console.log(car == 'subaru')
+// • Look closely at your results, and make sure you understand why each line evaluates to True or False.
+// • Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
+let fvrtActor = "Wahaj Ali";
+// 1st True
+console.log("Is fvrtActor === 'Wahaj Ali' ? It Predicts True");
+console.log(fvrtActor === 'Wahaj Ali');
+// 1st False
+console.log("Is fvrtActor === 'Yumna Zaidi' ? It Predicts False");
+console.log(fvrtActor === 'Yumna Zaidi');
+// 2nd True
+console.log("Is fvrtActor === 'Wahaj Ali' ? It Predicts True");
+console.log(fvrtActor === 'Wahaj Ali');
+// 2nd False
+console.log("Is fvrtActor === 'wahaj ali' ? It Predicts False");
+console.log(fvrtActor === 'wahaj ali');
+// 3rd True
+console.log("Is fvrtActor === 'Wahaj Ali' ? It Predicts True");
+console.log(fvrtActor === 'Wahaj Ali');
+// 3rd False
+console.log("Is fvrtActor === 'Imran Ashraf' ? It Predicts False");
+console.log(fvrtActor === 'Imran Ashraf');
+// 4th True
+console.log("Is fvrtActor === 'Wahaj Ali' ? It Predicts True");
+console.log(fvrtActor === 'Wahaj Ali');
+// 4th False
+console.log("Is fvrtActor === 'Sonya Hussyn' ? It Predicts False");
+console.log(fvrtActor === 'Sonya Hussyn');
+// 5th True
+console.log("Is fvrtActor === 'Wahaj Ali' ? It Predicts True");
+console.log(fvrtActor === 'Wahaj Ali');
+// 5th False
+console.log("Is fvrtActor === 'Kubra Khan' ? It Predicts False");
+console.log(fvrtActor === 'Kubra Khan');
