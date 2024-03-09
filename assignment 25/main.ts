80ae2c6068ff96e3c5ae4819f488b9185250bf77
Samiya Ayub <159205998+Samiyaayub@users.noreply.github.com>
2024-03-09 05:24:08 +0500

    Add files via upload

--- assignment 25/main.ts
+++ assignment 25/main.ts
@@ -1,1 +1,14 @@
+//Question 25: Alien Colors #1: Imagine an alien was just shot down in a game. Create a variable called alien_color and assign it a value of 'green', 'yellow', or 'red'.
+// • Write an if statement to test whether the alien’s color is green. If it is, print a message that the player just earned 5 points.
 
+// • Write one version of this program that passes the if test and another that fails. (The version that fails will have no output.)
+
+let aliens_color = "green";
+// • Write an if statement to test whether the alien’s color is green. If it is, print a message that the player just earned 5 points.
+if(aliens_color === "green") {
+     console.log("The Player just earned 5 points.\n");
+}
+// • Write one version of this program that passes the if test and another that fails. (The version that fails will have no output.)
+if(aliens_color === "red") {
+  console.log("The Player just earned 5 points.");
+} 
