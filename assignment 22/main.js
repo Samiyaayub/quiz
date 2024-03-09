7f9d6d1d0b45b814b382cfd885f7cfda6092777e
Samiya Ayub <159205998+Samiyaayub@users.noreply.github.com>
2024-03-09 15:39:09 +0500

    Add files via upload

---
+++ assignment 22/main.js
@@ -0,0 +1,6 @@
+"use strict";
+//Question 22: Intentional Error: If you haven’t received an array index error in one of your programs yet, try to make one happen. Change an index in one of your programs to produce an index error. Make sure you correct the error before closing the program.
+Object.defineProperty(exports, "__esModule", { value: true });
+let programmingLanguage = ["Python", "HTML", "CSS", "Javascript", "Typescript"];
+console.log(programmingLanguage[4]);
+console.log(programmingLanguage[3]);
--- assignment 22/main.ts
+++ assignment 22/main.ts
@@ -1,1 +1,7 @@
+//Question 22: Intentional Error: If you haven’t received an array index error in one of your programs yet, try to make one happen. Change an index in one of your programs to produce an index error. Make sure you correct the error before closing the program.
 
+let programmingLanguage: string[] = ["Python", "HTML", "CSS", "Javascript", "Typescript"];
+
+console.log(programmingLanguage[4]); 
+
+console.log(programmingLanguage[3]);
