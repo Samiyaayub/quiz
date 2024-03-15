cd5376c18c164997f1a6255d9be41bdd907392b7
Samiya Ayub <159205998+Samiyaayub@users.noreply.github.com>
2024-03-15 22:48:47 +0500

    Update main.ts

--- assignment 30/main.ts
+++ assignment 30/main.ts
@@ -3,12 +3,12 @@
 
 // • Otherwise, print a generic greeting, such as Hello Eric, thank you for logging in again.
 
-let employee: string[] =["Admin", "Olivia", "Isabella", "Camila", "Daisy"];
-for(let i = 0; i < employee.length; i++){
-    if(employee[i] == "admin"){
-        console.log("Hello admin, would you like to see a status report?")
+let usernames: string[] = ["admin", "user1", "user2", "user3", "user4"];
+
+usernames.forEach(username => {
+    if (username === "admin") {
+        console.log("Hello admin, would you like to see a status report?");
+    } else {
+        console.log(`Hello ${username}, thank you for logging in again.`);
     }
-    else{
-        console.log(`Hello ${employee[i]}`)
-    }
-}
+});
