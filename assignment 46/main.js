1511e25c73561cf44d7091af4bcc8af76e0ec19c
Samiya Ayub <159205998+Samiyaayub@users.noreply.github.com>
2024-03-18 19:34:08 +0500

    Add files via upload

---
+++ assignment 46/main.js
@@ -0,0 +1,11 @@
+//Question 45: Enhanced Laptop Object:Construct an object for a loptop properties make, model, year, and a method described() that logs a sentence about the laptop.
+let laptop = {
+    make: "Dell",
+    model: "XPS 13 plus",
+    year: 2023,
+    describe: function () {
+        console.log(`This laptop is a ${this.year} ${this.make} ${this.model}.`);
+    }
+};
+laptop.describe();
+export {};
--- assignment 46/main.ts
+++ assignment 46/main.ts
@@ -1,1 +1,12 @@
+//Question 46: Enhanced Laptop Object:Construct an object for a loptop properties make, model, year, and a method described() that logs a sentence about the laptop.
 
+
+let laptop = {
+    make: "Dell",
+    model: "XPS 13 plus",
+    year: 2023,
+    describe: function() {
+        console.log(`This laptop is a ${this.year} ${this.make} ${this.model}.`);
+    }
+};
+laptop.describe();
